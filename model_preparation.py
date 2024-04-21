b6be20267d52b5f7bb3b320ac8d7a78bac12d95a
ilonautkevich <149751920+ilonautkevich@users.noreply.github.com>
2024-04-21 22:45:27 +0500

    Create model_preparation.py

---
+++ model_preparation.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import pickle
+
+
+df_train = pd.read_csv('./train/result_train.csv')
+
+
+X_train = df_train.drop('target', axis=1)
+y_train = df_train['target']
+
+model = LinearRegression()
+print("--- Model created")
+
+model.fit(X_train, y_train)
+print("--- Model trained")
+
+pickle.dump(model, open('model.pkl', 'wb'))
+
